6212a5984a8e3afc5d7b2f8157bfe6e1df071465
dragomir <drkuleshov@yandex.ru>
2024-10-19 19:00:19 +0300

    lab2

--- app/src/main/java/com/example/myapplication/main.kt
+++ app/src/main/java/com/example/myapplication/main.kt
@@ -2,7 +2,17 @@ package com.example.myapplication
 
 fun main() {
     println("Hello")
-    //Задание 1
+    //Zadanie 1 variant 1
     println("Введите число")
-    val number =
+    val number = readln().toInt()
+    println(task1(number))
+    //zadanie 2 variant 1
+    val task2: (Int) -> Boolean = { number -> number > 0 }
+    println("Введите число")
+    val number1 = readln().toInt()
+    println(task2(number1))
+    //zadanie 3 variant 4
+    val array = arrayOf(-3, 5, 0, 7, -2, 10)
+    val arrayTask2 = array.filter(task2)
+    println(arrayTask2)
 }
--- app/src/main/java/com/example/myapplication/task1.kt
+++ app/src/main/java/com/example/myapplication/task1.kt
@@ -1,9 +1,11 @@
 package com.example.myapplication
 
-fun zadanie1v9(number: Int) :Int {
+
+fun task1(number: Int) :Int {
     return if (number < 0) {
         0
     } else {
         number * 2
     }
+
 }
