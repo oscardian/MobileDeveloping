98f88c0b5139ba94da4645ee63010c7e6f61a1c6
dragomir <drkuleshov@yandex.ru>
2024-10-19 18:27:52 +0300

    lab2

--- app/src/main/java/com/example/myapplication/main.kt
+++ app/src/main/java/com/example/myapplication/main.kt
@@ -2,4 +2,7 @@ package com.example.myapplication
 
 fun main() {
     println("Hello")
+    //Задание 1
+    println("Введите число")
+    val number =
 }
---
+++ app/src/main/java/com/example/myapplication/zadanie1v9.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication
+
+fun zadanie1v9(number: Int) :Int {
+    return if (number < 0) {
+        0
+    } else {
+        number * 2
+    }
+}
