6d31b01dfb262c33964c79be0476f1b47c225d20
dragomir <drkuleshov@yandex.ru>
2024-10-22 19:59:27 +0300

    Прогноз погоды 5. UI и ListItem

--- app/src/main/java/com/example/myapplication/screens/MainScreen.kt
+++ app/src/main/java/com/example/myapplication/screens/MainScreen.kt
@@ -5,9 +5,11 @@ import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -190,6 +192,13 @@ fun TabLayout() {
             state = pagerState,
             modifier = Modifier.weight(1.0f)
         ) { index ->
+            LazyColumn (
+                modifier = Modifier.fillMaxSize()
+            ){
+                items(15){
+                    ListItem()
+                }
+            }
         }
     }
 }
